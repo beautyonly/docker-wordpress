e759b233066d45e128e35196170ce2cd3178dd3f
beautyonly <38242987+beautyonly@users.noreply.github.com>
2018-05-18 18:50:50 +0800

    Update export.sh
    在数据库导出中添加bash shebang

--- export.sh
+++ export.sh
@@ -6,5 +6,5 @@ docker-compose exec db sh -c 'exec mysqldump "$MYSQL_DATABASE" -uroot -p"$MYSQL_
 if [[ $_os == "Darwin"* ]] ; then
   sed -i '.bak' 1,1d $_file
 else
-  sed -i 1,1d $_file # 接受错误信息：从文件中删除密码警告
+  sed -i 1,1d $_file # 接受错误信息从文件中删除密码警告
 fi
